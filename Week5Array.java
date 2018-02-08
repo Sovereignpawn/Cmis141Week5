41b0794657462f3aa75f70807f85b8836b3a2d36
Sovereignpawn <sovereignpawn@outlook.com>
2018-02-07 22:48:28 -0500

    First commit

---
+++ Week5Array.java
@@ -0,0 +1,23 @@
+/* File:      Week5Array.java
+ * Author:    Matthew Dwyer
+ * Date:      7 Feb 2018
+ * Purpose:   Demonstrate understanding of Arrays
+ */
+
+/**
+ * Provide Java code to create and initialize an array of integers (Java primitive int),
+ * floats or any other Java primitive type of your choice. You pick the array name and length.
+ * Demonstrate how you would determine the length of your array. Show this through a
+ * code example you create where you use the length member.
+ */
+
+public class Week5Array{
+  public static void main (String[] args){
+    int dwyer[]=new int[10];
+    dwyer[0]=87;
+    dwyer[9]=142;
+    System.out.println(dwyer[5]);
+
+  }//end main method
+}//end class
+
